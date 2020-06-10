6111957fe4b1947dc6279d530c02857b7d39caa8
Bryan Kribbs <bakribbs@us.ibm.com>
2020-06-10 00:18:40 -0500

    app built

---
+++ app.js
@@ -0,0 +1,23 @@
+var express = require('express');
+var app = express();
+app.get('/', function (req, res) {
+  res.status(200).send('Hello World!');
+});
+app.listen(3000, function () {
+  console.log('Example app listening on port 3000!');
+});
+
+
+app.get('/testers', function (req, res) {
+    res.status(200).send({ "firstName": "John",
+    "lastName" : "Doe",
+    "test": "Passed"
+     });
+  });
+
+app.get('/students', function (req, res) {
+    res.status(200).send({ "firstName": "Bill",
+    "lastName" : "Smith",
+    "studentID": 1
+        });
+});
