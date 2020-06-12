2cbea895d7e74cddab9a33aab5f5326f57d86294
Bryan Kribbs <bakribbs@us.ibm.com>
2020-06-12 13:12:52 -0500

    Updated Swagger

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ app.use(cors())
 app.options('*', cors())
 
 app.get('/', function (req, res) {
-  res.status(200).send('Hello World!');
+  res.status(200).send({"Hello": "World!"});
 });
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!');
