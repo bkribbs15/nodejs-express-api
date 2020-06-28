d39365b59100a1a055fde0ffb35d9d6683a03794
Bryan Kribbs <bkribbs15@gmail.com>
2020-06-28 16:24:43 -0500

    app fixes

--- app.js
+++ app.js
@@ -1,20 +1,26 @@
 var express = require('express');
 var app = express();
 var cors = require('cors')
+var path = require('path');
 
 app.use(cors())
 
 app.options('*', cors())
 
-app.get('/', function (req, res) {
-  res.status(200).send({"Hello": "World!"});
-});
 app.listen(3000, function () {
+  console.log(__dirname);
   console.log('Example app listening on port 3000!');
 });
 
+app.get('/', function(req, res) {
+  res.sendFile(path.join(__dirname + 'index.html'));
+});
 
 app.get('/testers', function (req, res) {
+    console.log({ "firstName": "John",
+    "lastName" : "Doe",
+    "test": "Passed"
+     });
     res.status(200).send({ "firstName": "John",
     "lastName" : "Doe",
     "test": "Passed"
@@ -22,8 +28,12 @@ app.get('/testers', function (req, res) {
   });
 
 app.get('/students', function (req, res) {
+    console.log({ "firstName": "Bill",
+    "lastName" : "Smith",
+    "studentID": 1
+     });
     res.status(200).send({ "firstName": "Bill",
     "lastName" : "Smith",
     "studentID": 1
-        });
+     });
 });
